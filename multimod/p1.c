88f46933a27553ff1b3b2e5f7af0276d67e7072e
tracer-nju <tracer@nju.edu.cn>
2019-12-19 17:03:00 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: b073b6a4d42d423ca783e8012561c913            Boot ID: f163e351f6674686b49e45597a164eb5     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  17:03:01 up 14:36,  5 users,  load average: 1.90, 1.62, 1.38

--- multimod/p1.c
+++ multimod/p1.c
@@ -1,7 +1,7 @@
 #include "multimod.h"
 
 int64_t multimod_p1(int64_t a, int64_t b, int64_t m) {
-   int64_t res =  ((a%m) * (b%m)) & m; 
+   int64_t res =  ((a%m) * (b%m)) % m; 
 
   // TODO: implement
   return res;
