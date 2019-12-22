9a0b0ff206a0969d87aa83e7dc9e19f8f8387052
tracer-nju <tracer@nju.edu.cn>
2019-12-22 21:40:15 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: b073b6a4d42d423ca783e8012561c913            Boot ID: f163e351f6674686b49e45597a164eb5     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  21:40:15 up 1 day,  5:03,  3 users,  load average: 0.71, 0.31, 0.16

--- multimod/p1.c
+++ multimod/p1.c
@@ -49,6 +49,7 @@ int64_t multimod_p1(int64_t a, int64_t b, int64_t m) {
 	   printf("str a_m:%s\n",str_a_m);
 	   printf("val b_m:%ld\n",b_m);
 	   printf("str b_m:%s\n",str_b_m);
+	   printf("str res:%s\n",str_res);
 	   for(int i = 0; i < len_b_m; i ++) {
 	       char bit_b = str_b_m[len_b_m-1-i];
 		   for(int j = 0; j < len_a_m; j ++) {
