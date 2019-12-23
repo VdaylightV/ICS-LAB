3ac6c683239e4fefc6da6f38417cc7fdaed21560
tracer-nju <tracer@nju.edu.cn>
2019-12-23 15:39:15 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: b073b6a4d42d423ca783e8012561c913            Boot ID: f163e351f6674686b49e45597a164eb5     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  15:39:15 up 1 day,  7:08,  3 users,  load average: 0.11, 0.09, 0.07

--- multimod/p1.c
+++ multimod/p1.c
@@ -93,7 +93,9 @@ int64_t multimod_p1(int64_t a, int64_t b, int64_t m) {
               
 			   else {  //若相乘结果大于9，此时需额外考虑进位
 				   int forward_bit = res/10;  //提取出相乘结果的十位
+			       printf("forward_bit:%d\n",forward_bit);
 				   int left_bit = res%10;     //提取出相乘结果的个位
+			       printf("left_bit:%d\n",left_bit);
 			       if(temp_res[len_a_m+len_b_m-1-i-j] + left_bit <= '9') { //若相乘结果的个位加到temp_res后不产生进位
 			           temp_res[len_a_m+len_b_m-1-i-j] += left_bit;        //那么forward_bit就是进位，放到前一位即可
 					   temp_res[len_a_m+len_b_m-1-i-j-1] += forward_bit;
