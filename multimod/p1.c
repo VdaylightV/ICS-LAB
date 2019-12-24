68967b25922044d1ff8b9bc5059caafaa2facbd5
tracer-nju <tracer@nju.edu.cn>
2019-12-24 16:45:55 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: b073b6a4d42d423ca783e8012561c913            Boot ID: f163e351f6674686b49e45597a164eb5     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  16:45:55 up 1 day, 14:40,  5 users,  load average: 0.34, 0.58, 0.52

--- multimod/p1.c
+++ multimod/p1.c
@@ -1,88 +1,6 @@
 #include "multimod.h"
 #include "stdio.h"
 #include "assert.h"
-/*
-static inline char* multi(char a[], int len_a, char b[], int len_b, char res[], int len_res) {
-	   // len_a_m = len_a - 1; len_b_m = len_b - 1
-	   // len_a_m+len_b_m = len_a+len_b-1
-	   char temp_res[len_a + len_b - 1];
-	   for(int i = 0; i < len_b-1; i ++) { //对应于下面一行的每一位
-
-	           for(int i = 0; i < len_a + len_b - 1; i ++) { //对应于上面一行的每一位
-	               temp_res[i] = '0';
-	           }
-	           temp_res[len_a + len_b -2] = '\0'; //由于竖式实际上是将下面的数的每一位去乘上面的数，因此，先把储存乘出来结果的数组清空
-
-	       char bit_b = str_b_m[len_b-1-1-i]; //竖式下面的那一行的每一位进行循环
-
-		   for(int j = 0; j < len_a - 1; j ++) {
-
-		       char bit_a = str_a_m[len_a-1-1-j]; //对下面竖式的每一位进行循环
-			   int res = (bit_a - '0') * (bit_b - '0'); //计算两位数相乘的结果
-			   
-               if(res <= 9) {
-			       if(temp_res[len_a+len_b-1-1-i-j] + res <= '9') {
-			           temp_res[len_a+len_b-1-1-i-j] += res;
-			       }	   //若乘出来的结果加到相应的temp_res位上，不产生进位，那么简单的temp_res位做加法即可
-			       else {
-			           temp_res[len_a+len_b-1-1-i-j] = res - 10 + temp_res[len_a+len_b-1-1-i-j];
-                       temp_res[len_a+len_b-1-1-i-j-1] += 1;
-			       }       //若产生进位，由于结果<=9那么至多进位为1，故前面的一位加1即可，后面的一位保留进位后的结果
-			   }               
-              
-			   else {  //若相乘结果大于9，此时需额外考虑进位
-				   int forward_bit = res/10;  //提取出相乘结果的十位
-				   int left_bit = res%10;     //提取出相乘结果的个位
-			       if(temp_res[len_a+len_b-1-1-i-j] + left_bit <= '9') { //若相乘结果的个位加到temp_res后不产生进位
-			           temp_res[len_a+len_b-1-1-i-j] += left_bit;        //那么forward_bit就是进位，放到前一位即可
-					   temp_res[len_a+len_b-1-1-i-j-1] += forward_bit;
-			       }	   
-			       else {                                                  //若相乘结果个位加到temp_res后产生进位 
-					   forward_bit += 1;                                   //此时的进位至多为1，forward_bit加1是进位
-					   temp_res[len_a+len_b-1-1-i-j] = temp_res[len_a+len_b-1-1-i-j] + left_bit - 10;
-					   temp_res[len_a+len_b-1-1-i-j-1] += forward_bit;
-			   }
-		   }
-		   }
-		   
-		   
-		   for(int k = 0; k < len_a + len_b-1; k ++) {
-		       int bit = temp_res[len_a + len_b-1-1-k] - '0';
-			   if(str_res[len_a + len_b-1-1-k] + bit <= '9') {
-			       str_res[len_a + len_b-1-1-k] += bit;
-			   }
-			   
-			   else {
-			       str_res[len_a + len_b-1-1-k] = str_res[len_a + len_b-1-1-k] + bit - 10;
-				   str_res[len_a + len_b-1-1-k-1] += 1;
-
-				   if(str_res[len_a + len_b-1-1-k-1] > '9') {
-				       str_res[len_a + len_b-1-1-k-1] -= 10;
-					   str_res[len_a + len_b-1-1-k-1-1] += 1;
-				   }
-				       if(str_res[len_a + len_b-1-1-k-1-1] > '9') {
-				           str_res[len_a + len_b-1-1-k-1-1] -= 10;
-					       str_res[len_a + len_b-1-1-k-1-1-1] += 1;
-				       }
-				           if(str_res[len_a + len_b-1-1-k-1-1-1] > '9') {
-				               str_res[len_a + len_b-1-1-k-1-1-1] -= 10;
-					           str_res[len_a + len_b-1-1-k-1-1-1-1] += 1;
-				           }
-				               if(str_res[len_a + len_b-1-1-k-1-1-1-1] > '9') {
-				                   str_res[len_a + len_b-1-1-k-1-1-1-1] -= 10;
-					               str_res[len_a + len_b-1-1-k-1-1-1-1-1] += 1;
-				               }
-				                   if(str_res[len_a + len_b-1-1-k-1-1-1-1-1] > '9') {
-				                       str_res[len_a + len_b-1-1-k-1-1-1-1-1] -= 10;
-					                   str_res[len_a + len_b-1-1-k-1-1-1-1-1-1] += 1;
-				                   }                                                         //这些看似非常傻的操作实际上确实非常傻，主要是为了处理烧熟连续进位的情况
-			   }
-
-			   }
-	   }
-    
-}
-*/
 
 static inline char* multi(char a[], int len_a, char b[], int len_b, char res[], int len_res) {   
 	   // len_a_m = len_a - 1; len_b_m = len_b - 1
@@ -205,6 +123,36 @@ static inline int compare(char a[], int len_a, char m[], int len_m) {
    }
 }
 
+static inline char* minus(char res[], char m[], int len) { // 默认a>b才能减
+    for(int i = 0; i < len-1; i ++) {
+		int bit_res = res[len-1-1-i];
+		int bit_m = m[len-1-1-i];
+		int gap = bit_res-bit_m;
+
+        if(gap >= 0) {
+		    res[len-1-1-i] = '0' + gap;
+		}
+                   
+		else {
+            int index = 1;
+			while(res[len-1-1-i-index] == '0') {
+                index ++;
+			}
+
+            res[len-1-1-i-index] -= 1;
+			    index --;
+ 
+				for(; index > 0; index --) {
+                    res[len-1-1-i-index] = '9';
+				   }
+
+				   res[len-1-1-i] = '0' + gap + 10;
+	   }
+       //printf("-----------temp res:%s-------------\n", res);
+   }
+   return res;
+}
+
 int64_t multimod_p1(int64_t a, int64_t b, int64_t m) {
 	int64_t res = 0;
 	if (m <= 2147483647) {
@@ -266,7 +214,7 @@ int64_t multimod_p1(int64_t a, int64_t b, int64_t m) {
        char* str_res_final;
        str_res_final = multi(str_a_m, len_a_m+1, str_b_m, len_b_m+1, str_res, len_a_m+len_b_m+1);
 
-       /*
+       
 	   char str_m[len_a_m+len_b_m+1];
 
 	   for(int i = 0; i < len_a_m + len_b_m; i ++) { //对应于上面一行的每一位
@@ -279,48 +227,24 @@ int64_t multimod_p1(int64_t a, int64_t b, int64_t m) {
 	       str_m[len_a_m + len_b_m - i - 1] = (temp % 10) + '0';
 		   temp /= 10;
 	   }
-	   */
-       /*
+
+       char* temp_res = str_res;   
 	   while(compare(str_res, len_a_m+len_b_m+1, str_m, len_a_m+len_b_m+1) >= 0) {
 	       if(compare(str_res, len_a_m+len_b_m+1, str_m, len_a_m+len_b_m+1) == 0) {
 		       res = 0;
 		   }
 		  
 		   else {
-		       for(int i = 0; i < len_a_m+len_b_m; i ++) {
-			       int bit_res = str_res[len_a_m+len_b_m-1-i];
-			       int bit_m = str_m[len_a_m+len_b_m-1-i];
-				   int gap = bit_res-bit_m;
-
-				   if(gap >= 0) {
-				       str_res[len_a_m+len_b_m-1-i] = '0' + gap;
-				   }
-                   
-				   else {
-
-					   int index = 1;
-					   while(str_res[len_a_m+len_b_m-1-i-index] == '0') {
-                           index ++;
-					   }
-
-					   str_res[len_a_m+len_b_m-1-i-index] -= 1;
-					   index --;
-
-					   for(; index > 0; index --) {
-                           str_res[len_a_m+len_b_m-1-i-index] = '9';
-					   }
-
-					   str_res[len_a_m+len_b_m-1-i] = '0' + gap + 10;
-				   }
-		   }
+			   temp_res = minus(temp_res, str_m, len_a_m+len_b_m+1);			   
+	   printf("temp left:%s\n",temp_res);
+		       }
 	   }
 	   
 
        //printf("mod_str:%s\n", str_m); 
        //printf("mod_val:%ld\n", m); 
-	   }
-	   */
-	   printf("res:%s\n",str_res_final);
+	  // }
+	   
 	   }
 	}
        
