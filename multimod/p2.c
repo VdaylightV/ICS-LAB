c5aea78f76ac8d7e455d6bf8a75a75cc1d0048e7
tracer-nju <tracer@nju.edu.cn>
2019-12-25 22:03:00 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: b073b6a4d42d423ca783e8012561c913            Boot ID: f163e351f6674686b49e45597a164eb5     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  22:03:00 up 2 days,  2:38, 10 users,  load average: 0.01, 0.12, 0.12

--- multimod/p2.c
+++ multimod/p2.c
@@ -1,6 +1,15 @@
 #include "multimod.h"
 
 int64_t multimod_p2(int64_t a, int64_t b, int64_t m) {
+  int64_t multi_factor = b;
+  int64_t left = 0;
+  while(a > 0) {
+      if(a&1 == 1) {
+	      left = (left + multi_factor) % m;
+	  }
+	  multi_factor = (multi_factor * multi_factor) % m;
+	  a >> 1;
+  } 
   // TODO: implement
-  return -2;
+  return left;
 }
