dd3b88f7307f50390e40715726c7e460a2834c49
tracer-nju <tracer@nju.edu.cn>
2019-12-25 17:21:26 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: b073b6a4d42d423ca783e8012561c913            Boot ID: f163e351f6674686b49e45597a164eb5     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  17:21:26 up 2 days, 2 min,  9 users,  load average: 0.14, 0.07, 0.10

--- multimod/p1.c
+++ multimod/p1.c
@@ -115,23 +115,28 @@ static inline int compare(char a[], int len_a, char m[], int len_m) { // 实际�
    } 
 
    if(bit_count_a < bit_count_m) {
+	   printf(">\n");
        return 1; // 返回1表示a大
    }
 
    else if(bit_count_a > bit_count_m) {
+	   printf("<\n");
        return -1; // 返回-1表示b大
    }
 
    else {
        for(int i = bit_count_a; i < len_a-1; i ++) {
 	       if(a[bit_count_a] > m[bit_count_m]) {
+	   printf(">\n");
 		       return 1;
 		   }
 
 	       else if(a[bit_count_a] < m[bit_count_m]) {
+	   printf("<\n");
 		       return -1;
 		   }
 	   }
+	   printf("=\n");
 	   return 0;
    }
 }
