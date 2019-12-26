fe7b2f73a8810f98cebacf736d21e5fb5b49bef4
tracer-nju <tracer@nju.edu.cn>
2019-12-26 14:00:54 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: b073b6a4d42d423ca783e8012561c913            Boot ID: a9f1b69766eb4019893233d6e8a49b55     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  14:00:54 up  1:00,  3 users,  load average: 0.52, 0.58, 0.27

--- multimod/p2.c
+++ multimod/p2.c
@@ -7,8 +7,8 @@ int64_t multimod_p2(int64_t a, int64_t b, int64_t m) {
   int64_t multi_factor = a;
   int64_t left = 0;
 
-  struct timeval tv0;
-  gettimeofday(&tv0, NULL);
+//  struct timeval tv0;
+//  gettimeofday(&tv0, NULL);
 
   while(b) {
       if(b&1) {
@@ -19,9 +19,9 @@ int64_t multimod_p2(int64_t a, int64_t b, int64_t m) {
   } 
   // TODO: implement
   
-  struct timeval tv1;
-  gettimeofday(&tv1, NULL);
-  printf("%lds\n",tv1.tv_usec-tv0.tv_usec);
+//  struct timeval tv1;
+//  gettimeofday(&tv1, NULL);
+//  printf("%lds\n",tv1.tv_usec-tv0.tv_usec);
   
   return left;
 }
