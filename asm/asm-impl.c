5337e26c397cd4dc05f8f80a565eaa9c64c89917
tracer-nju <tracer@nju.edu.cn>
2020-01-01 16:42:52 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: b073b6a4d42d423ca783e8012561c913            Boot ID: a9f1b69766eb4019893233d6e8a49b55     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  16:42:52 up  7:45,  2 users,  load average: 0.10, 0.06, 0.01

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -11,7 +11,8 @@ int64_t asm_add(int64_t a, int64_t b) {
           : "a"(m), "b"(n)
 		  : "%eax", "%ebx"
   );
-  printf("res:%ld\n",n);
+
+  printf("res:%ld\n",a);
 
   // TODO: implement
   return b;
