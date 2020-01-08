2014f07944ff36d8e43f5598f465ef4dd14fe01a
tracer-nju <tracer@nju.edu.cn>
2020-01-08 23:22:34 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: b073b6a4d42d423ca783e8012561c913            Boot ID: 9a16b519d1464ffab9ff03d02ed8e40e     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  23:22:35 up  6:47,  4 users,  load average: 0.06, 0.19, 0.26

--- multimod/p3.c
+++ multimod/p3.c
@@ -1,6 +1,15 @@
 #include "multimod.h"
 
 int64_t multimod_p3(int64_t a, int64_t b, int64_t m) {
+	struct timeval tv0;
+	gettimeofday(&tv0, NULL);
+
   int64_t t = (a * b - (int64_t)((double)a * b / m) * m) % m;
-  return t < 0 ? t + m : t;
+  t = t < 0 ? t + m : t;
+	struct timeval tv1;
+	gettimeofday(&tv1, NULL);
+	
+    printf("%ldms\n",tv1.tv_usec-tv0.tv_usec);
+
+  return t;
 }
