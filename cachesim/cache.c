5d47274c8004e909a0884af4524fe6ae75d51486
tracer-nju <tracer@nju.edu.cn>
2020-01-10 20:05:30 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: b073b6a4d42d423ca783e8012561c913            Boot ID: 9a16b519d1464ffab9ff03d02ed8e40e     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  20:05:30 up  9:03,  2 users,  load average: 0.10, 0.06, 0.03

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -32,7 +32,8 @@ uint32_t cache_read(uintptr_t addr) {
 	bool full = false; //用于判断对应的组(set)内是否已满
 	uint16_t mem_block_NO = 0; //用于记录主存块号
 
-	uint8_t block_inside_offset = ((addr & 0x3f) & ~0x3); //用于记录块内偏移量
+	uint8_t block_inside_offset = (addr & 0x3f); //用于记录块内偏移量
+//	uint8_t block_inside_offset = ((addr & 0x3f) & ~0x3); //用于记录块内偏移量
 //	printf("----READ-----||block_inside_offset:0x%x\n",(uint32_t)block_inside_offset);
 	uint8_t index = ((addr >> 6) & 0x3f); //用于cache组号
 //	printf("----READ-----||index:0x%x\n",(uint32_t)index);
@@ -123,7 +124,8 @@ void cache_write(uintptr_t addr, uint32_t data, uint32_t wmask) {
 	bool full = false; //用于判断对应的组(set)内是否已满
 	uint16_t mem_block_NO = 0; //用于记录主存块号
 
-	uint8_t block_inside_offset = ((addr & 0x3f) & ~0x3); //用于记录块内偏移量
+//	uint8_t block_inside_offset = ((addr & 0x3f) & ~0x3); //用于记录块内偏移量
+	uint8_t block_inside_offset = (addr & 0x3f); //用于记录块内偏移量
 //	printf("----WRITE-----||block_inside_offset:0x%x\n",(uint32_t)block_inside_offset);
 	uint8_t index = ((addr >> 6) & 0x3f); //用于cache组号
 //	printf("----WRITE-----||index:0x%x\n",(uint32_t)index);
