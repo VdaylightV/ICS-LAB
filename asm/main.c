d60aad6482de30285758ed659da00a483d53e978
tracer-nju <tracer@nju.edu.cn>
2020-01-12 11:24:24 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: b073b6a4d42d423ca783e8012561c913            Boot ID: 928d0ef210f64652b1a83dd724ff3b16     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  11:24:24 up 16:57,  4 users,  load average: 0.11, 0.15, 0.28

--- asm/main.c
+++ asm/main.c
@@ -56,8 +56,8 @@ static void setjmp_log(const char *str, int value) {
   printf("----head-str:%s\n", head->str);
   printf("----str:%s\n", str);
   assert(strcmp(head->str, str) == 0);
-  printf("----head-value:%s\n", head->value);
-  printf("----value:%s\n", value);
+  printf("----head-value:%d\n", head->value);
+  printf("----value:%d\n", value);
   assert(head->value == value);
   head++;
 }
