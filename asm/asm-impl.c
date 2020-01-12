7482b8a8e74ba34719890614bf677253a3ec1d7a
tracer-nju <tracer@nju.edu.cn>
2020-01-12 11:31:04 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: b073b6a4d42d423ca783e8012561c913            Boot ID: 928d0ef210f64652b1a83dd724ff3b16     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  11:31:04 up 17:04,  4 users,  load average: 0.19, 0.17, 0.24

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -111,7 +111,7 @@ int asm_setjmp(asm_jmp_buf env) {
 			"popfq;"
 			"movq (%%rsp), %%rax;"
 			"movq %%rax, 144(%1);"
-			"mov (%1), %%rax;"
+			"mov %0, %%rax;"
 			"retq;"
             
 		    :"=r"(ret_val)
