ec085ab1ee94ae2033d22296b321274ff9b124c4
tracer-nju <tracer@nju.edu.cn>
2020-01-12 15:31:41 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: b073b6a4d42d423ca783e8012561c913            Boot ID: 928d0ef210f64652b1a83dd724ff3b16     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  15:31:41 up 20:14,  2 users,  load average: 0.32, 0.29, 0.60

--- perf/main.c
+++ perf/main.c
@@ -97,7 +97,7 @@ static void run(void (*func)(), int rounds) {
     uint64_t ed = gettime();
 	printf("ed:%lu\n",ed);
     elapsed[round] = (ed - st);//CLOCKS_PER_SEC;
-	printf("CLOCKS_PER_SEC:%lu\d",CLOCKS_PER_SEC);
+	printf("CLOCKS_PER_SEC:%lu\n",CLOCKS_PER_SEC);
   }
 
   // TODO: display runtime statistics
