8b3f5c406d8b4c630ac0129638f29e6a46165857
tracer-nju <tracer@nju.edu.cn>
2020-01-12 16:54:45 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: b073b6a4d42d423ca783e8012561c913            Boot ID: 928d0ef210f64652b1a83dd724ff3b16     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  16:54:45 up 21:37,  2 users,  load average: 0.28, 0.22, 0.19

--- perf/impl.c
+++ perf/impl.c
@@ -19,6 +19,8 @@ void simple_loop() {
   for (volatile int i = 0; i < 1000000; i++) ;
 }
 
+extern int64_t multimod_p1(int64_t a, int64_t b, int64_t m); 
+
 extern int64_t multimod_p2(int64_t a, int64_t b, int64_t m); 
 
 extern int64_t multimod_p3(int64_t a, int64_t b, int64_t m); 
