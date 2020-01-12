8c191b272347833839b240d5939756dac200b521
tracer-nju <tracer@nju.edu.cn>
2020-01-12 17:30:00 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: b073b6a4d42d423ca783e8012561c913            Boot ID: 928d0ef210f64652b1a83dd724ff3b16     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  17:30:00 up 22:12,  3 users,  load average: 0.57, 0.90, 0.57

--- perf/p3.c
+++ perf/p3.c
@@ -5,6 +5,10 @@
 int64_t multimod_p3() {
 //	struct timeval tv0;
 //	gettimeofday(&tv0, NULL);
+  int64_t a = 89;
+  int64_t b = 23;
+  int64_t m = 34;
+
 
   int64_t t = (a * b - (int64_t)((double)a * b / m) * m) % m;
   t = t < 0 ? t + m : t;
