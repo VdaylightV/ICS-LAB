f2630d1f2e5adfb95c56d46c785b0b832d9bf65a
tracer-nju <tracer@nju.edu.cn>
2020-01-12 11:26:48 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: b073b6a4d42d423ca783e8012561c913            Boot ID: 928d0ef210f64652b1a83dd724ff3b16     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  11:26:48 up 17:00,  4 users,  load average: 0.23, 0.20, 0.27

--- asm/main.c
+++ asm/main.c
@@ -52,13 +52,13 @@ static struct log_entry expected[] = {
 static struct log_entry *head = expected;
 
 static void setjmp_log(const char *str, int value) {
-//  assert(head->str != NULL);
+  assert(head->str != NULL);
   printf("----head-str:%s\n", head->str);
   printf("----str:%s\n", str);
-  //assert(strcmp(head->str, str) == 0);
+  assert(strcmp(head->str, str) == 0);
   printf("----head-value:%d\n", head->value);
   printf("----value:%d\n", value);
-//  assert(head->value == value);
+  assert(head->value == value);
   head++;
 }
 
