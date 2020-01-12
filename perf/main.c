12ad83a372a081f3fb8004e020951ab114b4c3a2
tracer-nju <tracer@nju.edu.cn>
2020-01-12 21:45:20 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: b073b6a4d42d423ca783e8012561c913            Boot ID: 928d0ef210f64652b1a83dd724ff3b16     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  21:45:20 up 1 day, 54 min,  3 users,  load average: 0.28, 0.18, 0.24

--- perf/main.c
+++ perf/main.c
@@ -159,6 +159,6 @@ static void run(void (*func)(), int rounds) {
 //  printf("          average time : %fs\n", average);
 //  printf("          update average time : %fs\n", average_update);
   printf("%f\n", average_update);
-//  printf("          update variance : %f×10^(-8)\n", variance_update);
+  printf("          update variance : %f×10^(-8)\n", variance_update);
 //  printf("          bad points : %d\n", rounds-counts);
 }
