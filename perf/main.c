b72ed2430750704e57083fbad0176bb740cf5b30
tracer-nju <tracer@nju.edu.cn>
2020-01-12 15:34:16 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: b073b6a4d42d423ca783e8012561c913            Boot ID: 928d0ef210f64652b1a83dd724ff3b16     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  15:34:16 up 20:16,  2 users,  load average: 0.18, 0.24, 0.53

--- perf/main.c
+++ perf/main.c
@@ -84,7 +84,8 @@ static void (*lookup(const char *fn))() {
 
 static void run(void (*func)(), int rounds) {
   printf("rounds:%d\n", rounds);
-  uint64_t *elapsed = malloc(sizeof(uint64_t) * rounds);
+  //uint64_t *elapsed = malloc(sizeof(uint64_t) * rounds);
+  double *elapsed = malloc(sizeof(double) * rounds);
   if (!elapsed) {
     perror("elapsed");
     return;
@@ -96,7 +97,7 @@ static void run(void (*func)(), int rounds) {
     func();
     uint64_t ed = gettime();
 	printf("ed:%lu\n",ed);
-    elapsed[round] = (uint64_t)((double)ed - st)/(double)CLOCKS_PER_SEC;
+    elapsed[round] = (double)(ed - st)/(double)CLOCKS_PER_SEC;
 	printf("CLOCKS_PER_SEC:%lu\n",CLOCKS_PER_SEC);
   }
 
